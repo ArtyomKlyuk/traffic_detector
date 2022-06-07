89a2a405f1ed2d66f7cbf1f28fc8c15e4270990e
ArtyomKlyuk <artyom010408@gmail.com>
2022-06-07 23:28:38 +0300

    returning to first type of models

--- traffic_detector/detector/models.py
+++ traffic_detector/detector/models.py
@@ -1,12 +1,14 @@
-# Create your models here.
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-STATES = (('NEW', 'Новый'), ('SETUP', 'Настройка'), ('ACTIVE', 'Активный'))
+
+
+class ConformityCertificate(models.Model):
+    """Свидетельство средства измерения"""
+    number = models.CharField(verbose_name='Номер свидетельства', max_length=50)
+    expirationDate = models.DateField(verbose_name='Срок действия свидетельства в формате ISO8601', max_length=50)
 
 
 class Point(models.Model):
-    """Точка ГРЗ"""
     x = models.IntegerField(verbose_name='x координата')  # minimum: 0; maximum: 3840
     y = models.IntegerField(verbose_name='y координата')  # minimum: 0; maximum: 2160
 
@@ -18,26 +20,16 @@ class GpsCoord(models.Model):
 
 class Zone(models.Model):
     """Зона обзора"""
-    location = models.ForeignKey(GpsCoord, on_delete=models.CASCADE)
-    address_zone = models.CharField(verbose_name='Адрес зоны обзора', max_length=512)
+    location = models.ForeignKey()
+    address = models.CharField(verbose_name='Адрес зоны обзора', max_length=512)
     vrpDetectionArea = models.ForeignKey(Point, on_delete=models.CASCADE)
 
 
-class ConformityCertificate(models.Model):
-    """Свидетельство средства измерения"""
-    number = models.CharField(verbose_name='Номер свидетельства', max_length=50)
-    expirationDate = models.DateField(verbose_name='Срок действия свидетельства в формате ISO8601', max_length=50)
-
-
-class Detector(models.Model, Zone, GpsCoord):  # Надо сделать минимальную длину(validator)
-    """Детектор для приема данных"""
-    serialNumber = models.CharField(verbose_name='Серийный номер', max_length=50, )  # , min_length=6
+class Detector(models.Model):  # Надо сделать минимальную длину(validator)
+    serialNumber = models.CharField(verbose_name='Серийный номер', max_length=50)  # , min_length=6
     model = models.CharField(verbose_name='Производитель', max_length=50)  # , min_length=1
-    conformityCertificate = models.ForeignKey(ConformityCertificate, on_delete=models.CASCADE)
-    address_setup = models.CharField(verbose_name='Адрес установки', max_length=512)
-    gps_coord_device = models.ForeignKey(GpsCoord, on_delete=models.CASCADE)
-    # zone = Zone()
-    # zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
-    address = models.CharField(verbose_name='Адрес зоны обзора', max_length=512)
-    # vrpDetectionArea = Point()
-    state = models.CharField(verbose_name='Режим работы детектора', choices=STATES, max_length=20)
+    address = models.CharField(verbose_name='Адрес установки', max_length=512)
+    location = models.ForeignKey()
+    zone = models.ForeignKey()
+    STATES = ('NEW', 'SETUP', 'ACTIVE')
+    state = models.CharField(verbose_name='Режим работы детектора', choices=STATES)
